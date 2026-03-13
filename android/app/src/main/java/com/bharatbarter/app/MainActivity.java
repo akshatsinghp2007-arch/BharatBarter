beea76971278d92aa2ac0633bf404afb1b0cb208
akshatsinghp2007-arch <akshatsinghp2007@gmail.com>
2026-03-13 13:48:10 +0530

    changed it to apk file

---
+++ android/app/src/main/java/com/bharatbarter/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bharatbarter.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bharatbarter.app',
+  appName: 'BharatBarter',
+  webDir: 'dist'
+};
+
+export default config;
